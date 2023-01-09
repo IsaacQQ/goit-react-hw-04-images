bb8442ddcab5d8da905d0303584034c75324eb00
Игорь <bujhtyz228@gmail.com>
2023-01-10 01:36:05 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '110vh',
+        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
